23fe9423dbf2651e9bb68014a2d927bd3ce12996
roxyzc <roxyzc12@gmail.com>
2023-05-10 23:07:16 +0700

    first commit

---
+++ app/Controllers/Page.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        return view('about', [
+            'title' => 'Halaman About',
+            'content' => 'Ini adalah halaman about yang menjelaskan tentang isi
+       halaman ini.'
+        ]);
+    }
+
+    public function contact()
+    {
+        return view('contact', [
+            'title' => 'Halaman contact',
+            'content' => 'Ini adalah halaman contact'
+        ]);
+    }
+
+    public function articel()
+    {
+        return view('articel', [
+            'title' => 'Halaman articel',
+            'content' => 'Ini adalah halaman articel'
+        ]);
+    }
+
+    public function home()
+    {
+        return view('home', [
+            'title' => 'Halaman home',
+            'content' => 'Ini adalah halaman home'
+        ]);
+    }
+}
